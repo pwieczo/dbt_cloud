f4f3574c93eda63def5204ed5158a4ab397b2300
Piotr Wieczorek <piotr.tech.1@gmail.com>
2023-06-10 06:13:10 +0000

    developing stage orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,24 @@
+select  
+         o.orderid
+        , o.ORDERDATE
+        , o.SHIPDATE
+        , o.SHIPMODE
+        , o.ORDERSELLINGPRICE - o.ORDERCOSTPRICE as orderprofit
+        , o.ORDERSELLINGPRICE
+        , o.ORDERCOSTPRICE
+        , c.CUSTOMERNAME
+        , c.SEGMENT
+        , c.COUNTRY
+        , p.CATEGORY
+        , p.PRODUCTNAME
+        , p.SUBCATEGORY
+from
+        {{ ref('raw_order') }} as o
+left join
+        {{ ref('raw_customer') }} as c
+on
+        o.CUSTOMERID = c.CUSTOMERID
+left join 
+        {{ ref('raw_product') }} as p
+on  
+        o.PRODUCTID = p.PRODUCTID
